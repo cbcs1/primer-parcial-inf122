b6360c1dd6f7f596e675851853e5dc863b60bd02
cbcs1 <cchavezs@fcpn.edu.bo>
2024-01-11 12:03:43 -0400

    examen

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function mensaje() {
+  const elem = document.getElementById("first-button");
+  elem.windowAlert('DESCARGANDO ARCHIVO');
+}
+function directorio(){
+  const elem=getElementById("second-button");
+  elem.windowAlert('s/c');
+}
